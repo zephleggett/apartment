19b73f7c7ccdf6e236134df1689ae98e4158a064
zephleggett <zephleggett@gmail.com>
2021-02-02 15:59:47 -0600

    require migrator monkey patch in apartment

--- lib/apartment/active_record/migrator.rb
+++ lib/apartment/active_record/migrator.rb
@@ -1,12 +1,10 @@
 module ActiveRecord
-  class Migrator < ActiveRecord # :nodoc:
-    class << self
-      def generate_migrator_advisory_lock_id
-        hash_input = ActiveRecord::Base.connection.current_database
-        hash_input += ActiveRecord::Base.connection.current_schema if ActiveRecord::Base.connection.respond_to?(:current_schema)
-        db_name_hash = Zlib.crc32(hash_input)
-        MIGRATOR_SALT * db_name_hash
-      end
+  class Migrator # :nodoc:
+    def generate_migrator_advisory_lock_id
+      hash_input = ActiveRecord::Base.connection.current_database
+      hash_input += ActiveRecord::Base.connection.current_schema if ActiveRecord::Base.connection.respond_to?(:current_schema)
+      db_name_hash = Zlib.crc32(hash_input)
+      MIGRATOR_SALT * db_name_hash
     end
   end
 end
